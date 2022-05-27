b25ef327853fab6124346e724b5a62af2fa28126
Jason Robinson <json.robinson@gmail.com>
2022-05-27 09:34:10 -0500

    Using macros to comment out template
    Cleaner than block comments and easier to turn them on / off.
    Also reflects Schemes comment macro #;

---
+++ src/mixed-data.lisp
@@ -0,0 +1,198 @@
+;;; -*- Mode: Lisp; Syntax: Common Lisp; Package: mixed -*-
+;;; 
+;;; HtDP 1e Exercise 7.2.1
+;;; ============================================================================
+
+(defpackage mixed
+  (:documentation "Collection of Zoo Animals.")
+  (:use :cl)
+  (:export
+   ;; Zoo
+   :fits-p
+   #:spider #:make-spider #:copy-spider #:spider-p #:spider-legs #:spider-space
+   #:elephant #:make-elephant #:copy-elephant #:elephant-p #:elephant-space
+   #:monkey #:make-monkey #:copy-monkey #:monkey-p #:monkey-intelligence #:monkey-spacev
+
+   ;; Vehicles
+   :bus :make-bus :bus-p :bus-route :bus-doors :bus-capacity :bus-has-conducter-p 
+   ))
+
+(in-package :mixed)
+
+;; =============================================================================
+;;;; DATA DEFINITIONS
+
+;; ======= Zoo Animals
+
+;; An Animal is one of:
+;; - Spider
+;; - Elephant
+;; - Monkey
+;; Interpretation: Represents a type of Zoo Animal and it's characteristics.
+
+;; A Cage is a Number
+;; Interpretation: Describes the volume of a cage in Cubit Inches
+
+;; A Spider is a structure:
+;;   (make-spider :legs Number :space Number)
+;; Interpretation: <See Documentation>
+(defstruct spider
+  ":LEGS are the number of remaining legs (we assume that spiders can lose legs
+in accidents) and the
+:SPACE they need in case of transport."
+  (legs 8)
+  space)
+
+;; EXAMPLES: See Tests
+
+;; TEMPLATE:
+
+#+(or)
+;; fn-for-spider : Spider -> ???
+(defun fn-for-spider (s)
+  (values 
+   (spider-legs s)
+   (spider-space s)))
+
+;; An Elephant is a structure:
+;;   (make-elephant :space Number)
+;; Interpretation: <See Documentation>
+(defstruct elephant
+  "An elephants only attribute are the :SPACE they need in case of transportation.
+:SPACE is a Number representing Cubit Feet"
+  (space 400))
+
+;; EXAMPLES: See Tests
+
+;; TEMPLATE:
+
+#+(or)
+;; fn-for-elephant : Elephant -> ???
+(defun fn-for-elephant (e)
+  (elephant-space e))
+
+
+;; A Monkey is a structure:
+;;   (make-monkey :intelligence Number :space Number)
+;; Interpretation: See documentation
+(defstruct monkey
+  ":INTELLIGENCE is a number between 0-100 and :SPACE needed for transportation."
+  intelligence
+  (space 20))
+
+;; TEMPLATE:
+
+#+(or)
+;; fn-for-monkey : Monkey -> ???
+(defun fn-for-monkey (m)
+  (values
+   (monkey-intelligence m)
+   (monkey-space m)))
+
+;; ======= Vehicles
+
+;; A Vehicle is one of:
+;; - buses
+;; - limos
+;; - cars
+;; - subways
+;; Interpretation: Fleets of vehicles managed by metropolitan transportation
+;;                 agencies.
+
+;; A Bus is a structure:
+;;   (make-bus route doors capacity has-conducter-p)
+;; where: ROUTE is a Symbol, DOORS and PASSENGERS are Numbers and HAS-CONDUCTER-P
+;;        is a Boolean.
+(defstruct bus
+  "Represents a bus and its passenger capacity."
+  doors capacity has-conducter-p)
+
+;; TEMPLATE:
+
+#+(and)
+;; fn-for-bus : Bus -> ???
+(defun fn-for-bus (b)
+  (values
+   (bus-doors b)
+   (bus-capacity b)
+   (bus-has-conducter-p b)))
+
+;; A Limo is a structure
+;;   (make-limo has-sunroof-p length colour)
+;; where: has-sunroof-p is a Boolean, Length is a Number and colour is a Symbol
+(defstruct limo
+  (has-sunroof-p t)
+  length
+  colour)
+
+;; TEMPLATE:
+
+#+(and)
+;; fn-for-limo : Limo -> ???
+(defun fn-for-limo (l)
+  (values
+   (limo-has-sunroof-p l)
+   (limo-length l)
+   (limo-colour l)))
+
+;; A Car is a structure:
+;;   (make-car type doors colour cc has-turbo-p)
+;; where: type is a Symbol of 'sedan 'convertible 'hatch-back
+;;        doors is a Number, COLOUR is a Symbol, cc is a Number and has-turbo-p
+;;        is a Boolean
+(defstruct car
+  type doors colour cc
+  (has-turbo-p nil))
+
+;; TEMPLATE:
+
+#+(and)
+;; fn-for-car : Car -> ???
+(defun fn-for-car (c)
+  (values
+   (car-type c)
+   (car-doors c)
+   (car-colour c)
+   (car-cc c)
+   (car-has-turbo-p c)))
+
+;; A Train is a structure:
+;;   (make-train route length type has-conducter-p)
+;; where: ROUTE is a Symbol, LENGTH and capacity are Numbers, TYPE is a Symbol of
+;;        either 'tram, 'bullet, 'steam or 'subway and HAS-CONDUCTER-P is a Boolean
+(defstruct train
+  route
+  length
+  (type 'subway)
+  (has-conducter-p nil))
+
+;; TEMPLATE:
+
+#+(and)
+;; fn-for-train : Train -> ???
+(defun fn-for-train (train)
+  (values
+   (train-route train)
+   (train-length train)
+   (train-capacity train)
+   (train-type train)
+   (train-has-conducter-p train)))
+
+;; =============================================================================
+;;;; FUNCTIONS
+
+;; ======= Zoo Animals
+
+;; fits-p : Animal Cage -> Boolean
+;; Determines whether the cage is large enough for the given animal
+;; (defun fits-p (animal cage) (declare (ignore animal cage)) nil) ; stub
+;; <template taken from various animals>
+
+(defun fits-p (animal cage)
+  "With the given Zoo Animal and the volume of the cage, determin whether the
+cage is large enough for the animal.
+Does a spider take up less space with less legs?"
+  (typecase animal
+    (spider (> cage (spider-space animal)))
+    (elephant (> cage (elephant-space animal)))
+    (monkey (> cage (monkey-space animal)))))
